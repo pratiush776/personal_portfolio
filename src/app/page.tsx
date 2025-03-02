f384f024d1984ecf35cfad006a73bf35f3c2eedf
pratiush776 <pratiush776@gmail.com>
2025-03-02 13:51:59 -0500

    window_prerender_fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import Hero from "@/components/Hero";
 import NavPrimary from "@/components/NavPrimary";
 import Content from "@/components/content";
 import { ReactLenis } from "lenis/react";
+import { useEffect } from "react";
 
 export default function Home() {
-  window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  };
+  useEffect(() => {
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
   return (
     <ReactLenis root options={{ lerp: 0.75 }}>
       <main className="bg-[#E6E6E6] dark:bg-[#555454] scroll-smooth">
